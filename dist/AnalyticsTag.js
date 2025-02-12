b789b5ab637bbeae9fe53a9643b64f02303bf985
callumtw42 <callumtw42@gmail.com>
2025-02-12 13:46:17 +0700

    - build

--- dist/AnalyticsTag.js
+++ dist/AnalyticsTag.js
@@ -229,7 +229,7 @@ function getUserData() {
                         };
                     });
                     console.log(userData);
-                    return [4 /*yield*/, fetch('http://localhost:3000/analytics', {
+                    return [4 /*yield*/, fetch('http://localhost:3000/pageLog', {
                             method: 'POST',
                             body: JSON.stringify(userData)
                         })];
